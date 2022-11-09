fb68a50fd357a844c868414ce355e34f2a72ea95
Yarin <59511153+YarinCardillo@users.noreply.github.com>
2022-11-09 23:21:36 +0100

    liste1.c

---
+++ liste1.c
@@ -0,0 +1,26 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+typedef int Item;
+typedef struct node node_t, *link;
+
+struct node{
+    Item val;
+    link next;
+};
+
+int main(){
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    else
+    return 1;
+    
+    
+    
+
+    
+
+    return 0;
+}
